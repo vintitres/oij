e09b40cef6ff88c2cc1c99c142bd2e5561f10793
Jakub Woyke <jaqb@sekta.pl>
2022-08-03 20:36:44 +0000

    mess

--- mno.cpp
+++ mno.cpp
@@ -9,22 +9,37 @@ bool cmp(long long i1, long long i2) {
 
 using namespace std;
 
-map<pair<long long, short>, short> bestM;
+map<pair<long long, short>, pair<short,short>> bestM;
 vector<long long> klocki,maxx;
+  /*short findkunder(long long x, short b, short e) {
+  int m = (b + e) / 2;
+  if (x 
+}
+pair<short,short> findk*/
 short best(long long x, short k){
   //  cout << x << " " << k << endl;
   if (x == 1) return 0;
-  if (k == -1 /*|| x > maxx[k]*/) return 200;
+  if (k == -1 || x > maxx[k]) return 200;
+  if (klocki[k] == x) {
+    //bestM[make_pair(x, k)] = 1;
+    return 1;
+  }
+  /*{
+    if (klocki[k] > x) {
+      
+    return best(x, findkunder(x/2, k));
+  }*/
   auto it = bestM.find(make_pair(x,k));
-  if (it != bestM.end()) return it->second;
+  if (it != bestM.end()) return it->second.first;
   short b = best(x, k - 1);
   if (x % klocki[k] == 0LL) {
     short bb =  best(x / klocki[k], k -1) + 1;
     b = bb < b ? bb : b;
   }
-  bestM[make_pair(x,k)] = b;
+  bestM[make_pair(x,k)] = make_pair(b, 0);
   return b;
 }
+
 int main() {
   short k;
   long long x;
@@ -35,8 +50,18 @@ int main() {
     klocki.push_back(kl);
   }
   sort(klocki.begin(),klocki.end(), cmp);
+  long long mx = 1;
   for (int i =0 ; i < k; ++i){
-    maxx.push_back((i> 0 ? maxx[i - 1] : 1LL) * klocki[i]);
+    if (mx <= x) {
+      long long mxx = mx * klocki[i];
+      unsigned long long mxxx = (unsigned long long)mx * (unsigned long long)klocki[i];
+      if (mxx / mx != klocki[i] || mxxx / (unsigned long long)mx  != (unsigned long long)klocki[i]) {
+        mx = x + 1;
+      } else {
+        mx = mxx;
+      }
+    }
+    maxx.push_back(mx);
   }
   short b = best(x, k - 1);
   if (b > k){
@@ -44,18 +69,26 @@ int main() {
     return 0;
   }
   cout<< b <<endl;
-  //return 0;
+  /*return 0;
   --k;
   while (x> 1){
     b = bestM[make_pair(x,k)];
     while (true) {
       long long kl = klocki[k--];
-      if (x%kl==0 && bestM[make_pair(x/kl,k)] + 1 == b) {
+      if (x%kl==0) {
+        auto it = bestM.find(make_pair(x/kl,k));
+        if (it != bestM.end() && it->second + 1 == b) {
         cout << kl << " ";
         x/=kl;
         break;
+        }
       }
     }
+    if (b == 2) {
+      cout << x << endl;
+      break;
+    }
   }
+  */
 
 }
