f2c2feef9e3a8090430d6e001a94a3e10c8bb0af
Jakub Woyke <jaqb@sekta.pl>
2022-08-03 15:02:50 +0000

    mnoo

--- mno.cpp
+++ mno.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
+#include<algorithm>
 #include<map>
 #include<vector>
 #include<climits>
+bool cmp(long long i1, long long i2) {
+  return i1 > i2;
+}
+
 using namespace std;
 
 map<pair<long long, short>, short> bestM;
-vector<long long> klocki;
+vector<long long> klocki,maxx;
 short best(long long x, short k){
   //  cout << x << " " << k << endl;
   if (x == 1) return 0;
-  if (k == -1) return 200;
+  if (k == -1 /*|| x > maxx[k]*/) return 200;
   auto it = bestM.find(make_pair(x,k));
   if (it != bestM.end()) return it->second;
   short b = best(x, k - 1);
@@ -29,12 +34,17 @@ int main() {
     cin >>kl;
     klocki.push_back(kl);
   }
+  sort(klocki.begin(),klocki.end(), cmp);
+  for (int i =0 ; i < k; ++i){
+    maxx.push_back((i> 0 ? maxx[i - 1] : 1LL) * klocki[i]);
+  }
   short b = best(x, k - 1);
   if (b > k){
     cout << "NIE" << endl;
     return 0;
   }
   cout<< b <<endl;
+  //return 0;
   --k;
   while (x> 1){
     b = bestM[make_pair(x,k)];
